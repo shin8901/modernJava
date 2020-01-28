fad5877686957afedabab4eb1c9ce7d602ea524b
신운재 <woonjae.shin@miracom.co.kr>
2020-01-28 21:21:07 +0900

    rrrrrrrr

--- inActionJava/src/inActionJava/chapter02/ApplePredicate.java
+++ inActionJava/src/inActionJava/chapter02/ApplePredicate.java
@@ -3,3 +3,20 @@ package inActionJava.chapter02;
 public interface ApplePredicate {
 	boolean test(Apple apple);
 }
+
+class AppleHeavyWeightPredicate implements ApplePredicate {
+
+	@Override
+	public boolean test(Apple apple) {
+		return apple.getWeight() > 150;
+	}
+
+}
+
+class AppleGreenColorPredicate implements ApplePredicate {
+
+	@Override
+	public boolean test(Apple apple) {
+		return Color.GREEN.equals(apple.getColor());
+	}
+}
