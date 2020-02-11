93e847b8fa04e0a3f112593db5b45590e45a0793
WJ <WJ@DESKTOP-9MG8010>
2020-02-11 17:42:26 +0900

    아 근데 양이 많다

---
+++ src/main/java/chapter03/InterfaceTest.java
@@ -0,0 +1,11 @@
+package chapter03;
+
+@FunctionalInterface
+public interface InterfaceTest {
+	public void add(int a, int b);
+}
+
+//@FunctionalInterface
+//interface InterfaceTest2 extends InterfaceTest {
+//	public void add2(int a, int b);
+//}
