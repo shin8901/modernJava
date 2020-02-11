e4416e6e6f8953a05af27f513711d37a3555456b
WJ <WJ@DESKTOP-9MG8010>
2020-02-11 10:33:51 +0900

    ddddd

---
+++ src/main/java/chapter03/Dish.java
@@ -0,0 +1,24 @@
+package chapter03;
+
+public class Dish {
+
+	private int calories;
+	private String name;
+
+	public int getCalories() {
+		return calories;
+	}
+
+	public void setCalories(int calories) {
+		this.calories = calories;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
